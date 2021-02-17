004d931a2ee2b84f47f8fa7dece4a269a76957aa
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-02-17 15:18:07 +0900

    '버블정렬'

--- 알고리즘개념정리/BubbleSort.py
+++ 알고리즘개념정리/BubbleSort.py
@@ -12,6 +12,7 @@ def bubblesort(data):
     
     for index in range(len(data)-1):
         swap = False
+        #이부분이 중요 java에서는 -1 안햇는데 헷갈릴 수 있음
         for index2 in range(len(data)-index-1):
             if data[index2]>data[index2+1]:
                 data[index2],data[index2+1]=data[index2+1],data[index2]
