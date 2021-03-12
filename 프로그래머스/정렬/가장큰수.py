62ee40a219df27690f02190d73b4517dc46c28ab
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-03-12 14:44:05 +0900

    Add (이진탐색) : 케이스 추가

---
+++ 프로그래머스/정렬/가장큰수.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    answer = ''
+    visit = list()
+    for i in range(len(numbers)):
+        max_value = str(numbers[i])[0]
+        visited.append(max_value)
+        while len(visit)==len(numbers):
+            abs
+
+
+    return answer
+solution([3, 30, 34, 5, 9])
