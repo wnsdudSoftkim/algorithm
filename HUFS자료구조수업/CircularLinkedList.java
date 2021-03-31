c39e72e03b11abab414a5d4a6ebd6af8d896e61b
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-03-31 11:34:26 +0900

    Add (자료구조수업) : CircularLinkedList

---
+++ HUFS자료구조수업/CircularLinkedList.java
@@ -0,0 +1,27 @@
+package HUFS자료구조수업;
+class ListNode{
+    private ListNode link;
+}
+public class CircularLinkedList {
+    //자유공간 리스트
+    //원형연결 리스트
+    private ListNode head;
+    private int length;
+    public CircularLinkedList() {
+        head = null;
+        length=0;
+    }
+    public void insertFront(ListNode p) {
+        if(head ==null) {
+            head = p;
+            p.link = head;
+        }else {
+            p.link = head.link;
+            head.link = p;
+        }
+        length++;
+    }
+
+
+}
+
