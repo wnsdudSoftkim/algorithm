718f66d7ec8673c1e99a55713face2f2e559e333
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-04-03 01:00:53 +0900

    Add (Assignment) : LinkedList

--- HUFS자료구조수업/Assignment/Assignment3.java
+++ HUFS자료구조수업/Assignment/Assignment3.java
@@ -15,59 +15,98 @@ class DoubleLinkedList {
 	public DoubleLinkedList() {
 		head = tail = null; // 리스트가 비어있을 때는 모두 null 이다.
 	}
-	/**
-	 * @param str 삽입하고자 하는 데이터
-	 * 작성하여야 한다.
-	 */
+
 		/**
 	 * @param str 삽입하고자 하는 데이터
 	 * 작성하여야 한다.
 	 */
-	
-	/**
-	 * @param str 삽입하고자 하는 데이터
-	 * 작성하여야 한다.
-	 */
 	public int count=0;
+	String[] array = new String[100];
 	public void insert(String str) {
-		ListNode Node = new ListNode();
-		Node.data = str;
-		Node.llink = Node.rlink = Node;
-		if(head==null) {
-			head = tail = Node;
-			tail.rlink =null;
-			head.rlink = null;
-		}else {
-			tail.rlink = Node;
-			tail = Node;
-			tail.rlink = null;
+		ListNode p = head;
+		ListNode mynode = new ListNode();
+		mynode.data = str;
+		mynode.llink = mynode.rlink;
+		array[count] = str;
+		// if(count==0) {
+		// 	head=tail=mynode;
+		// }else if(count==1) {
+		// 	//기존 데이터가 왼쪽
+		// 	if(p.data.compareTo(str)<0) {
+		// 		p.rlink = mynode.llink;
+		// 		tail = mynode;
+		// 		tail.rlink = null;
+		// 	}else {
+		// 		head = mynode;
+		// 		head.rlink = p;
+		// 		tail = p;
+		// 	}
+		// }else {
+		if(count ==0) head =tail = mynode;
+		else {
+			for(int i=0; i<=count-1; i++) {
+				for(int j= i; j<=count; j++) {
+					//앞이 더 크면
+					if (array[i].compareTo(array[j])>0) {
+						String temp = array[i];
+						array[i] = array[j];
+						array[j] = temp;
+						
+					}
+				}
+			}
+			head.data = array[0];
+			head.llink =null;
+			tail = head;
+			for(int i=1; i<=count; i++) {
+				ListNode a = new ListNode();
+				a.data = array[i];
+				a.llink = tail;
+				a.llink.rlink = a;
+				tail = a;
+			}
 		}
-		count++;
-		sort();
+		
+		count ++;
+		
+	
+		
+		
+		
+		
+		
+		
+// 		while(p!=null) {
+// 			//p의 데이터가 앞에 있어야 되면
+// 			if(p.data.compareTo(mynode.data)<0) {
+// 				if(p.rlink ==null) {
+// 					p.rlink = mynode.llink;
+// 					tail = mynode;
+// 					tail.rlink = null;
+// 				}else {
+// 						mynode.rlink = p.rlink.llink;
+// 						p.rlink = mynode.llink;
+// 				}
+// 			}//p가 뒤에 있어야 되면
+// 			else {
+// 				if(p.llink == null) {
+// 					head=mynode;
+// 					head.llink = null;
+// 					head.rlink = p;
+// 				}else {
+// 					p.llink.rlink = mynode.llink;
+// 					p.llink = mynode.rlink;
+// 				}
+				
+				
+// 			}
+// 			p = p.rlink;
+// 		}
+		
+		
+		
 	}
 
-	public void sort() {
-		String[] myArray = new String[count];
-		ListNode p = head;
-		for(int i=0; i<count; i++) {
-			if(p==null) break;
-			myArray[i] = p.data;
-			p=p.rlink;
-		}
-		for(int i=0; i<count-1; i++) {
-			if(((int)myArray[i].charAt(0)) < ((int)myArray[i+1].charAt(0))) {
-				String temp = myArray[i];
-				myArray[i] = myArray[i+1];
-				myArray[i+1] = temp;
-			}
-				
-		}
-		for(int i=0; i<count; i++) {
-			if(p==null) break;
-			p.data = myArray[i];
-			p=p.rlink;
-		}
-	}
 
 	/**
 	 * @param str 삭제하고자 하는 데이터
@@ -75,11 +114,13 @@ class DoubleLinkedList {
 	 */
 	public void delete(String str) {
 		ListNode p = head;
+		if (p== null) return;
 		while(true) {
 			if(!p.data.equals(str)) {
 				p = p.rlink;
 			}else break;
 		}
+		
 		if(p ==head ) {
 			if(p.rlink ==null) head=null;
 			else {
@@ -93,9 +134,21 @@ class DoubleLinkedList {
 			p.llink.rlink = p.rlink;
 			p.rlink.llink = p.llink;
 		}
+		int a=0;
+		for (int i=0; i<count; i++) {
+			if(array[i].equals(str)) {
+				a = i;
+			}
+		}
+		for(int i=a; i<count; i++) {
+			array[i] = array[i+1];
+		}
 		count--;
 		
 	}
+
+	
+
 
 	// 저장된 모든 데이터를 출력한다.
 	public void print() {
@@ -106,7 +159,7 @@ class DoubleLinkedList {
 
 		String str = "";
 		for(ListNode p = head; p != null; p = p.rlink)
-			System.out.print(p.data);
+			System.out.print(p.data = " ");
 		System.out.println();
 	}
 }
---
+++ HUFS자료구조수업/Assignment/test.java
@@ -0,0 +1,7 @@
+package HUFS자료구조수업.Assignment;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("john".compareTo("tom"));
+    }
+}
